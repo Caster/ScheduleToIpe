ae2ec7f13b2a36558e34f579ae226dcd7116024e
Barnabbas <stefvs92@gmail.com>
2012-11-21 15:49:18 +0100

    Completed Schedule.
    Also changed some small things in the API, like the name of lcm/getLcm
    function and the changed the return type of getTasks to a Set.

--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -1,6 +1,8 @@
 
 package Model
 
+import java.util.Collections;
+
 /**
  * The Schedule object.
  * This object contains information about our schedule.
@@ -9,21 +11,50 @@ package Model
  */
 public class Schedule {
 	
+	private final Set<Task> tasks;
+	private final List<Task> taskSchedule;
+	
+	/**
+	 * Constructs a new Schedule.<br>
+	 * The schedule will consists of the Tasks in {@code schedule} such that 
+	 * {@code getTask(i) == schedule.get(i)}.
+	 * When there is no Task at a certain moment, then {@code null} has to be placed there.
+	 * The length of {@code schedule} indicates the LCM for this Schedule.
+	 * 
+	 * @param schedule the schedule for the Tasks.
+	 */
+	Schedule(List<Task> schedule){
+		// using unmodifiables to guarantee immutability.
+		taskSchedule = Collections.unmodifiableList(new ArrayList(schedule));
+		tasks = new Collections.unmodifiableSet(new HashSet<Task>(schedule));
+	}
+	
 	/**
 	 * Gets the tasks of this Schedule.
 	 * @return a List containing the tasks of this Schedule.
 	 */
-	public List<Task> getTasks(){
-		return null;
+	public Set<Task> getTasks(){
+		return tasks;
 	}
 	
 	/**
 	 * The task that runs immediatly after the given time.
+	 * Returns {@code null} when no Task will run at that moment. <br>
+	 * {@code time} must be smaller then the LCM of this Schedule.
+	 * 
+	 * 
 	 * @param time the timestamp of the moment you want the task of.
-	 * @return the Task that runs immediatly after the given timestamp.
+	 * @return the Task that runs immediatly after the given timestamp or 
+	 * {@code null} if there is no such Task.
+	 * 
+	 * @throws IllegalArgumentException if {@code time} is larger then the LCM
 	 */
-	public Task getTaskAt(int time){
-		return null;
+	public Task getTaskAt(int time) throws IllegalArgumentException{
+		if (time >= taskSchedule.size()){
+			throw new IllegalArgumentException("The given time is not within lcm, time is " + time);
+		}
+		
+		return taskSchedule.get(time);
 	}
 	
 	/**
@@ -31,8 +62,9 @@ public class Schedule {
 	 * This is the least common multiplier of the periods of the Tasks.
 	 * @return Length of a cycle of this Schedule.
 	 */
-	public int lcm(){
-		return null;
+	public int getLcm(){
+		// this assumes that the given taskSchedule is valid
+		return taskSchedule.size();
 	}
 
 }
