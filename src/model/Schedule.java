da2a838aa22a316be9ad6d3e1d9357b710abaad5
Barnabbas <stefvs92@gmail.com>
2012-11-21 15:26:13 +0100

    Created functions + API for Schedule.
    No implementations and constructors yet.

--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -4,10 +4,35 @@ package Model
 /**
  * The Schedule object.
  * This object contains information about our schedule.
+ * 
  * @author Barnabbas
  */
 public class Schedule {
 	
-	public 
+	/**
+	 * Gets the tasks of this Schedule.
+	 * @return a List containing the tasks of this Schedule.
+	 */
+	public List<Task> getTasks(){
+		return null;
+	}
+	
+	/**
+	 * The task that runs immediatly after the given time.
+	 * @param time the timestamp of the moment you want the task of.
+	 * @return the Task that runs immediatly after the given timestamp.
+	 */
+	public Task getTaskAt(int time){
+		return null;
+	}
+	
+	/**
+	 * The length of a cycle of this Schedule.<br>
+	 * This is the least common multiplier of the periods of the Tasks.
+	 * @return Length of a cycle of this Schedule.
+	 */
+	public int lcm(){
+		return null;
+	}
 
 }
--- src/model/Task.java
+++ src/model/Task.java
@@ -5,5 +5,43 @@
  *
  */
 public class Task {
+	
+	
+	/**
+	 * The name of this Task.
+	 * @return The name of this Task.
+	 */
+	public String getName(){
+		
+	}
+	
+	/**
+	 * The period of this Task.
+	 * @return The period of this Task.
+	 */
+	public int getPeriod(){
+		
+	}
+	
+	/**
+	 * The relative deadline of this Task. 
+	 * @return the relative deadline of this Task.
+	 */
+	public int getDeadline(){
+		
+	}
+	
+	/**
+	 * The execution time of this Task. That is the running time of each Task instance.
+	 * @return the execution time of this Task.
+	 */
+	public int getExecutionTime(){
+		
+	}
+	
+	@override
+	public String toString(){
+		return "Task " + getName();
+	}
 
 }
