3ef4c5fcdc999290d03f7b849a046d3e28491352
Thom Castermans <thomcastermans@users.sourceforge.net>
2012-11-26 11:26:44 +0100

    Made number of numbers along X-axis more precise.
    When the user asks for X numbers along the X-axis, we want to have X numbers. Not X-1, or X+1, but just X. Now, it is better.

--- src/output/OutputIpe.java
+++ src/output/OutputIpe.java
@@ -105,7 +105,7 @@ public class OutputIpe {
 
 		// Some variable declarations
 		TaskInstance curTaskInstance, prevTaskInstance = null;
-		int i, j;
+		int j;
 
 		// Ipe header
 		outputHeader();
@@ -194,12 +194,13 @@ public class OutputIpe {
 			String postfix = options.getStringOption("xAxisPostLabelText");
 			
 			// writing the numbers
-			int stepSize = xAxisNumbering < 0 ? 1 : (schedule.getLcm() / (xAxisNumbering - 1));
+			double stepSize = xAxisNumbering < 0 ? 1 : ((double) schedule.getLcm() / (xAxisNumbering - 1));
 			int maxOption = options.getIntegerOption("scheduleMaxLength");
 			int until = maxOption == 0 ? schedule.getLcm() : maxOption;
-			for (i = 0; i <= until; i += stepSize) {
-				writeString(prefix + i + postfix, OFFSET_X
-						+ GRID_SIZE * i, OFFSET_Y - GRID_SIZE * tasks.size()
+			for (double i = 0; i <= until; i += stepSize) {
+				int writeAt = (int) Math.ceil(i);
+				writeString(prefix + writeAt + postfix, OFFSET_X
+						+ GRID_SIZE * writeAt, OFFSET_Y - GRID_SIZE * tasks.size()
 						- TEXT_MARGIN, "center", "top");
 			}
 			// always draw the last
